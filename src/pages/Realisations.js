0b50f4eb9eea586d1e534db3d15352c71d18187a
Aziz BENNANE <ext.aziz.bennane@reseau.sncf.fr>
2024-07-28 22:28:52 +0200

    feat : Update Réalisations page

--- src/pages/Realisations.js
+++ src/pages/Realisations.js
@@ -10,17 +10,8 @@ function Realisations() {
             <div className="row gy-4 py-2" data-aos="zoom-in">
                 <div className="col-lg-4">
                     <div className="card bg-transparent">
-                        <h4 className="py-1">Blog</h4>
-                        <p className="para-light">Blog pour des développeurs débutants à confirmés.<br />- <u>Stack</u> : Drupal 9, PHP 7.4</p>
-                        <div className="my-3">
-                            <a className="btn float-end" target="_blank" rel="noopener noreferrer" href="https://blog.aziz-bennane.fr">Visiter</a>
-                        </div>
-                    </div>                    
-                </div>
-                <div className="col-lg-4">
-                    <div className="card bg-transparent">
                         <h4 className="py-1">My Spend</h4>
-                        <p className="para-light">Outil de gestion des dépenses et charges mensuelles.<br />- <u>Stack</u> : Symfony 5.2, Vuejs 2</p>
+                        <p className="para-light">Outil de gestion des dépenses et des charges mensuelles selon le profil connecté.<br /><br />- <u>Stack</u> : Symfony 5.2, Vuejs 2</p>
                         <div className="my-3">
                             <a className="btn float-end" target="_blank" rel="noopener noreferrer" href="https://myspend.aziz-bennane.fr">Visiter</a>
                         </div>
@@ -29,7 +20,7 @@ function Realisations() {
                 <div className="col-lg-4">
                     <div className="card bg-transparent">
                         <h4 className="py-1">Geolocip</h4>
-                        <p className="para-light">Ce site permet de géolocaliser des adresses IP publiques.<br />- <u>Stack</u> : React 17, Leaflet 1.7</p>
+                        <p className="para-light">Ce site web permet de géolocaliser des adresses IP publiques sur cartes Leaflet.<br /><br />- <u>Stack</u> : React 17, Leaflet 1.7</p>
                         <div className="my-3">
                             <a className="btn float-end" target="_blank" rel="noopener noreferrer" href="https://geolocip.aziz-bennane.fr">Visiter</a>
                         </div>
@@ -38,11 +29,20 @@ function Realisations() {
                 <div className="col-lg-4">
                     <div className="card bg-transparent">
                         <h4 className="py-1">Pricing</h4>
-                        <p className="para-light">Cet outil a pour objectif de simuler le placement d'un produit face à la concurrence.<br />- <u>Stack</u> : Symfony 4.3, Twig</p>
+                        <p className="para-light">Outil de simulation de placement d'un produit face à la concurrence.<br /><br />- <u>Stack</u> : Symfony 4.3, Twig</p>
                         <div className="my-3">
                             <a className="btn float-end" target="_blank" rel="noopener noreferrer" href="https://pricing.aziz-bennane.fr">Visiter</a>
                         </div>
                     </div>
+                </div>
+                <div className="col-lg-4">
+                    <div className="card bg-transparent">
+                        <h4 className="py-1">CHR 44</h4>
+                        <p className="para-light">Site web pour une association de Courses Hippiques Rurales, avec d'administration.<br /><br />- <u>Stack</u> : Next 14, React 18, Redux, tailwindCSS</p>
+                        <div className="my-3">
+                            <a className="btn float-end" target="_blank" rel="noopener noreferrer" href="https://chr44.aziz-bennane.fr">Visiter</a>
+                        </div>
+                    </div>                    
                 </div>
             </div> 
         </div> 
